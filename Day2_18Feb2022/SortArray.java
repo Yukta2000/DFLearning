3a117d5a587ad4c97b9cbf7226e425b49fa496ff
Yukta2000 <64189617+Yukta2000@users.noreply.github.com>
2022-02-24 18:04:46 +0530

    Create SortArray.java

---
+++ Day2_18Feb2022/SortArray.java
@@ -0,0 +1,13 @@
+//Sort integer array
+import java.util.Arrays;
+public class MyClass {
+    private static int[] sort_intarr(int[] numarr){
+        Arrays.sort(numarr);
+        return numarr;
+    }
+    public static void main(String args[]) {
+      int [] numarr ={13, 7, 6, 45, 21, 9, 2, 100 };
+      
+      System.out.println(Arrays.toString(sort_intarr(numarr)));
+    }
+}
